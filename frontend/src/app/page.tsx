a3a6a5b90eec8d4781af6fc0a45be57a598ed279
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-04-09 21:26:26 +0200

    Added signup guide

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Courses from '../components/Courses';
+import Signup from '../components/Signup';
 export default function Home() {
   return (
       <>
@@ -14,6 +15,7 @@ export default function Home() {
         <Header />
         <Hero />
           <Courses />
+          <Signup />
         <main className="pt-20">
           {/* Your homepage content */}
           <section id="kursy">
