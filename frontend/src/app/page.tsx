752110b58b97691a1291390787c13f9e5f4acc61
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-04-09 19:01:24 +0200

    Added header

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+// @ts-ignore
+import Header from '../components/Header';
+
+export default function Home() {
+  return (
+      <>
+        <Head>
+          <title>SuperPrawoJazdy.pl</title>
+          <meta name="description" content="Driving School website" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <Header />
+
+        <main className="pt-20">
+          {/* Your homepage content */}
+          <section id="kursy">
+            <h2>Kursy</h2>
+            {/* Add course info here */}
+          </section>
+
+          <section id="zapisy">
+            <h2>Jak się zapisać</h2>
+            {/* Add sign-up info here */}
+          </section>
+
+          {/* Other sections */}
+        </main>
+      </>
+  );
+}
