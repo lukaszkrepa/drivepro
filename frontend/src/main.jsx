ea76a74e23928c7872842da8caede6c6d25c5f24
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-04-11 23:38:32 +0200

    Added gallery

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero.jsx";
 import Courses from "./components/Courses.jsx";
 import SignupSteps from "./components/SignupSteps.jsx";
 import TeamAndCars from "./components/TeamAndCars.jsx";
+import Gallery from "./components/Gallery.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,5 +15,6 @@ createRoot(document.getElementById('root')).render(
       <Courses/>
       <SignupSteps/>
       <TeamAndCars/>
+        <Gallery/>
   </StrictMode>,
 )
