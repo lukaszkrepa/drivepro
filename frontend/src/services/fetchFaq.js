151f9281dc5fbc9d8ac18578117ef4db46e36aca
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-05-20 09:55:34 +0200

    Added FAQ database support

---
+++ frontend/src/services/fetchFaq.js
@@ -0,0 +1,19 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+    ScanCommand,
+    DynamoDBDocumentClient,
+} from "@aws-sdk/lib-dynamodb";
+import { awsConfig } from "../config/awsConfig";
+
+const client = new DynamoDBClient(awsConfig);
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+export async function fetchFaq() {
+    const command = new ScanCommand({
+        TableName: "FAQ",
+    });
+
+    const response = await ddbDocClient.send(command);
+
+    return response.Items;
+}
