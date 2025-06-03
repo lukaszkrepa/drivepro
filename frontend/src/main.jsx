e96a4d6f34ac6c71cd27f3ec782d77f40e8b5943
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-06-04 01:37:41 +0200

    fixed issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import awsExports from './aws-exports';
 import { Amplify } from 'aws-amplify';
 
 createRoot(document.getElementById('root')).render(
