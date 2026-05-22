7913eb43d36bf9f195d0552c2209da22432bb4cd
LukaszKrepa <lukaszkrepa@yahoo.com>
2026-05-22 11:17:27 +0100

    Update Amplify outputs for dev backend

---
+++ frontend/src/amplify-config.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+// WARNING: DO NOT EDIT. This file is generated from ../../amplify_outputs.json.
+
+export const apiBaseUrl = 'https://je9f6510o7.execute-api.eu-west-1.amazonaws.com';
--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,6 +1,7 @@
 import { fetchAuthSession } from 'aws-amplify/auth';
+import { apiBaseUrl } from '../amplify-config';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = apiBaseUrl;
 
 async function getAuthToken() {
   const session = await fetchAuthSession();
